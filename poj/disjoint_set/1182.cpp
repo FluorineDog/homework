a47243966cbdaa07d6f8e8da02b83f396611d690
FluorineDog <fluorine@gmail.com>
2017-12-13 03:04:09 +0800

    farmat

--- poj/disjoint_set/1182.cpp
+++ poj/disjoint_set/1182.cpp
@@ -1,62 +1,4 @@
-#include <stdio.h>
-namespace dog_util {
-class InputStream {
-public:
-  void redirect(const char *filename) {
-#ifdef DEBUG
-    //"disjoint_set/1733.in"
-    freopen(filename, "r", stdin);
-#endif
-  }
-
-  InputStream &operator>>(int &k) { scanf("%d", &k); }
-  InputStream &operator>>(long long &k) { scanf("%lld", &k); }
-  InputStream &operator>>(unsigned &k) { scanf("%u", &k); }
-  InputStream &operator>>(unsigned long long &k) { scanf("%llu", &k); }
-  InputStream &operator>>(char *str) { scanf("%s", str); }
-} cin;
-
-class OutputStream {
-public:
-  OutputStream &operator<<(int k) { printf("%d", k); }
-  OutputStream &operator<<(long long k) { printf("%lld", k); }
-  OutputStream &operator<<(unsigned k) { printf("%u", k); }
-  OutputStream &operator<<(unsigned long long k) { printf("%llu", k); }
-  OutputStream &operator<<(const char *str) { printf("%s", str); }
-} cout;
-
-class DebugStream {
-public:
-  int errprintf(const char *str, ...) {
-#ifdef DEBUG
-    fprintf(stderr, str, va_list);
-#endif
-  }
-  DebugStream &operator<<(int k) { errprintf("%d", k); }
-  DebugStream &operator<<(long long k) { errprintf("%lld", k); }
-  DebugStream &operator<<(unsigned k) { errprintf("%u", k); }
-  DebugStream &operator<<(unsigned long long k) { errprintf("%llu", k); }
-  DebugStream &operator<<(const char *str) { errprintf("%s", str); }
-} cerr;
-const char *endl = "\n";
-}
-typedef long long ll;
-typedef long long ull;
-using dog_util::cin;
-using dog_util::cout;
-using dog_util::cerr;
-using dog_util::endl;
-
-#include <map>
-#include <set>
-#include <stdio.h>
-#include <vector>
-using std::vector;
-using std::pair;
-using std::set;
-using std::map;
-using std::make_pair;
-
+#include "../wheel.h"
 struct Data {
   int i;
   int type;
@@ -83,13 +25,14 @@ bool insert(vector<Data> &fa, int X, int Y, int op) {
   if ((yd.type - xd.type + 3) % 3 != op) {
     return false;
   }
+  return true;
 }
 
 int main() {
   int N, K;
   cin >> N >> K;
   vector<Data> fa(N);
-  for (int i = 0; i < fa.size(); ++i) {
+  for (int i = 0; i < (int)fa.size(); ++i) {
     fa[i] = Data(i, 0);
   }
 
@@ -104,7 +47,7 @@ int main() {
       ++lie_count;
       continue;
     }
-    auto status = insert(fa, X, Y, op);
+    bool status = insert(fa, X, Y, op);
     if (!status) {
       ++lie_count;
     }
--- poj/disjoint_set/1417.cpp
+++ poj/disjoint_set/1417.cpp
@@ -1,151 +1,4 @@
-// #undef DOG_DEBUG
-#include <stdarg.h>
-#include <stdio.h>
-
-namespace dog_util {
-class InputStream {
-public:
-  void redirect(const char *filename) {
-#ifdef DOG_DEBUG
-    freopen(filename, "r", stdin);
-#endif
-  }
-
-  // easy to make mistake
-  // if insist, use getchar() instead
-  /* InputStream &operator<<(char &k) { scanf("%c", k); } */
-
-  // better
-  InputStream &operator<<(char &k) {
-    char buf[2];
-    scanf("%s", buf);
-    k = buf[0];
-    return *this;
-  }
-
-  InputStream &operator>>(int &k) {
-    scanf("%d", &k);
-    return *this;
-  }
-  InputStream &operator>>(long long &k) {
-    scanf("%lld", &k);
-    return *this;
-  }
-  InputStream &operator>>(unsigned &k) {
-    scanf("%u", &k);
-    return *this;
-  }
-  InputStream &operator>>(unsigned long long &k) {
-    scanf("%llu", &k);
-    return *this;
-  }
-  InputStream &operator>>(char *str) {
-    scanf("%s", str);
-    return *this;
-  }
-  InputStream &operator>>(double &k) {
-    scanf("%f", k);
-    return *this;
-  }
-} cin;
-void poj_throw() {
-  int *memory = new int[1UL << 31];
-  memory = new int[1UL << 31];
-  memory = new int[1UL << 31];
-  memory = new int[1UL << 31];
-}
-
-class OutputStream {
-public:
-  OutputStream &operator<<(char k) {
-    printf("%c", k);
-    return *this;
-  }
-  OutputStream &operator<<(int k) {
-    printf("%d", k);
-    return *this;
-  }
-  OutputStream &operator<<(long long k) {
-    printf("%lld", k);
-    return *this;
-  }
-  OutputStream &operator<<(unsigned k) {
-    printf("%u", k);
-    return *this;
-  }
-  OutputStream &operator<<(unsigned long long k) {
-    printf("%llu", k);
-    return *this;
-  }
-  OutputStream &operator<<(const char *str) {
-    printf("%s", str);
-    return *this;
-  }
-  OutputStream &operator<<(double k) {
-    printf("%f", k);
-    return *this;
-  }
-} cout;
-
-class DebugStream {
-public:
-  int errprintf(const char *str, ...) {
-#ifdef DOG_DEBUG
-    va_list argptr;
-    va_start(argptr, str);
-    vfprintf(stderr, str, argptr);
-    va_end(argptr);
-#endif
-  }
-  DebugStream &operator<<(char k) {
-    errprintf("%c", k);
-    return *this;
-  }
-  DebugStream &operator<<(int k) {
-    errprintf("%d", k);
-    return *this;
-  }
-  DebugStream &operator<<(long long k) {
-    errprintf("%lld", k);
-    return *this;
-  }
-  DebugStream &operator<<(unsigned k) {
-    errprintf("%u", k);
-    return *this;
-  }
-  DebugStream &operator<<(unsigned long long k) {
-    errprintf("%llu", k);
-    return *this;
-  }
-  DebugStream &operator<<(const char *str) {
-    errprintf("%s", str);
-    return *this;
-  }
-  DebugStream &operator<<(double k) {
-    errprintf("%f", k);
-    return *this;
-  }
-} cerr;
-const char *endl = "\n";
-}
-using dog_util::cin;
-using dog_util::cout;
-using dog_util::cerr;
-using dog_util::endl;
-using dog_util::poj_throw;
-typedef long long ll;
-typedef long long ull;
-
-#include <algorithm>
-#include <map>
-#include <set>
-#include <stdio.h>
-#include <vector>
-using std::vector;
-using std::pair;
-using std::set;
-using std::map;
-using std::make_pair;
+#include "../wheel.h"
 struct FUCK {
   int m[2];
   operator int *() { return m; }
@@ -170,8 +23,8 @@ Data find(vector<Data> &fa, int i) {
 }
 void log(vector<Data> &fa) {
 #ifdef DOG_DEBUG
-  typedef vector<Data>::iterator Iter;
-  for (int i = 0; i < fa.size(); ++i) {
+  // typedef vector<Data>::iterator Iter;
+  for (int i = 0; i < (int)fa.size(); ++i) {
     cerr << "{" << i << "->" << fa[i].i << "(" << fa[i].type << ")}";
   }
   // for (Iter iter = fa.begin(); iter != fa.end(); ++iter) {
@@ -194,7 +47,6 @@ bool deal() {
   for (int i = 0; i < N; ++i) {
     fa[i] = Data(i, 0);
   }
-  int lie_count = 0;
   for (int index = 0; index < K; ++index) {
     int X, Y;
     char buf[10];
@@ -216,7 +68,7 @@ bool deal() {
   }
   log(fa);
   map<int, FUCK> table;
-  for (int i = 0; i < fa.size(); ++i) {
+  for (int i = 0; i < (int)fa.size(); ++i) {
     Data data = find(fa, i);
     int *tmp = table[data.i];
     tmp[data.type]++;
@@ -288,13 +140,14 @@ bool deal() {
       poj_throw();
     }
   }
-  for (int i = 0; i < fa.size(); ++i) {
+  for (int i = 0; i < (int)fa.size(); ++i) {
     int divine_type = divine_0set.find(fa[i].i) != divine_0set.end() ? 0 : 1;
     if (divine_type == fa[i].type) {
       cout << i + 1 << endl;
     }
   }
   cout << "end" << endl;
+  return true;
 }
 
 int main() {
