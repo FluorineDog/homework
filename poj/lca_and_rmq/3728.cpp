bc4958b01ae0f1a645694903a15a87cb18922e71
FluorineDog <fluorine@gmail.com>
2018-01-03 16:37:20 +0800

    cp

--- poj/lca_and_rmq/3728.cpp
+++ poj/lca_and_rmq/3728.cpp
@@ -1,11 +1,15 @@
 #include "../wheel.h"
 struct Data{
+	int ancestor;
 	int max;
 	int min;
 	int up_gap;
 	int down_gap; 
+	Data(int loc, int value):ancestor(loc), max(value), min(value), up_gap(0), down_gap(0){
+		
+	}
 	Data append_up(Data& v){
-		Data tmp;
+		Data tmp(v.ancestor, 0);
 		tmp.max = std::max(max, v.max);
 		tmp.min = std::min(min, v.min);
 		tmp.up_gap = std::max(std::max(tmp.up_gap, up_gap), max-v.min);
@@ -16,6 +20,7 @@ struct Vertex {
   // int ancestor;
   // int ancestor_depth;
 	int depth;
+	vector<Data> jmpTable;
   int discover_time;
   int finish_time;
   vector<int> neighbors;
@@ -50,6 +55,7 @@ private:
     if (graph[source].discover_time == -1) {
       Vertex &vertex = graph[source];
       vertex.discover_time = timestamp++;
+			vertex.depth = 
       for (int i = 0; i < (int)vertex.neighbors.size(); ++i) {
         dfs(graph, vertex.neighbors[i]);
       }
