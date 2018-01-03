e31fc742ce9656eaf2d840c802700abbf1855dbe
FluorineDog <fluorine@gmail.com>
2018-01-03 17:28:35 +0800

    cp

--- poj/lca_and_rmq/3728.cpp
+++ poj/lca_and_rmq/3728.cpp
@@ -1,26 +1,28 @@
 #include "../wheel.h"
-struct Data{
-	int ancestor;
-	int max;
-	int min;
-	int up_gap;
-	int down_gap; 
-	Data(int loc, int value):ancestor(loc), max(value), min(value), up_gap(0), down_gap(0){
-		
-	}
-	Data append_up(Data& v){
-		Data tmp(v.ancestor, 0);
-		tmp.max = std::max(max, v.max);
-		tmp.min = std::min(min, v.min);
-		tmp.up_gap = std::max(std::max(tmp.up_gap, up_gap), max-v.min);
-		tmp.down_gap = std::max(std::max(tmp.down_gap,down_gap), v.max-min);
-	}
+struct Data {
+  int ancestor;
+  int max;
+  int min;
+  int up_gap;
+  int down_gap;
+  Data(int loc, int value)
+      : ancestor(loc), max(value), min(value), up_gap(0), down_gap(0) {}
+  Data append_up(const Data &v) const {
+    Data tmp(v.ancestor, 0);
+    tmp.max = std::max(max, v.max);
+    tmp.min = std::min(min, v.min);
+    tmp.up_gap = std::max(std::max(tmp.up_gap, up_gap), max - v.min);
+    tmp.down_gap = std::max(std::max(tmp.down_gap, down_gap), v.max - min);
+  }
+  int blend_dest(const Data &v) {
+    return std::max(std::max(up_gap, v.down_gap), max - v.min);
+  }
 };
 struct Vertex {
-  // int ancestor;
-  // int ancestor_depth;
-	int depth;
-	vector<Data> jmpTable;
+  int ancestor;
+	int price;
+  int depth;
+  vector<Data> jmpTable;
   int discover_time;
   int finish_time;
   vector<int> neighbors;
@@ -36,7 +38,7 @@ public:
     }
     timestamp = 0;
     depth = -1;
-    dfs(graph, source);
+    dfs(graph, source, -1);
   }
   void operator()(Graph &graph) {
     for (int i = 0; i < (int)graph.size(); ++i) {
@@ -45,19 +47,32 @@ public:
     timestamp = 0;
     for (int source = 0; source < (int)graph.size(); ++source) {
       depth = -1;
-      dfs(graph, source);
+      dfs(graph, source, -1);
     }
   }
 
 private:
-  void dfs(Graph &graph, int source) {
+  void dfs(Graph &graph, int source, int parent) {
     ++depth;
     if (graph[source].discover_time == -1) {
       Vertex &vertex = graph[source];
       vertex.discover_time = timestamp++;
-			vertex.depth = 
+      vertex.depth = depth;
+      {
+				Data acc = Data(parent, vertex.price);
+				vertex.jmpTable.push_back(acc);
+				// [beg 2^k] = [beg 2^--k] * [beg double 2^--k]
+				int cur_beg = acc.ancestor;
+				int k = 0; 
+				while(cur_beg - (1 << k) >= 0){
+					acc = acc.append_up(graph[cur_beg].jmpTable[k]);
+					vertex.jmpTable.push_back(acc);
+					++k;
+					cur_beg = acc.ancestor;
+				}
+			}
       for (int i = 0; i < (int)vertex.neighbors.size(); ++i) {
-        dfs(graph, vertex.neighbors[i]);
+        dfs(graph, vertex.neighbors[i], source);
       }
       vertex.finish_time = timestamp;
     }
@@ -68,3 +83,7 @@ private:
   int depth;
   int timestamp;
 };
+
+int main(){
+	
+}
