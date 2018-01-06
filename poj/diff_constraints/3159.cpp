a1c05eeec9292979e016c8c985d016507d472476
FluorineDog <fluorine@gmail.com>
2018-01-06 16:38:54 +0800

    buggy

--- poj/diff_constraints/3159.cpp
+++ poj/diff_constraints/3159.cpp
@@ -1,7 +1,25 @@
+// #define HIGH_IO_PERFORMANCE
 #include "../wheel.h"
-
+#define ull int
+#include "graph.h"
 int main() {
-	int M, N;
-	cin >> N >> N;
-	
+  cin.redirect("data.in");
+  int M, N;
+  cin >> N >> M;
+  Graph graph(N + 1);
+  while (M-- > 0) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    graph.add_edge(u, v, w);
+  }
+  graph.spfa(1);
+  ull first = graph[N].value;
+  // for (int i = 1; i <= N; ++i) {
+  //   graph[i].value = 1 << 30;
+  // }
+  // graph.spfa(N);
+  // ull second = graph[1].value;
+  // cout << std::max(first, second) << endl;
+  cout << first << endl;
+  return 0;
 }
--- poj/diff_constraints/graph.h
+++ poj/diff_constraints/graph.h
@@ -4,7 +4,7 @@ struct Vertex {
   ull value;
   int edge_beg;
   int visited;
-  Vertex() : value(1 << 30), edge_beg(0), visited(0) {}
+  Vertex() : value(1LL << 30), edge_beg(0), visited(0) {}
 };
 
 class Graph : public vector<Vertex> {
@@ -12,21 +12,21 @@ public:
   Graph(int V, int E = 0) {
     this->resize(V);
     edges.reserve(E + 1);
-    edges.push_back(Edge(-1, -1));
+    edges.push_back(Edge(-1, -1, 0));
     // jmpTable.resize(V);
   }
 
   struct Edge {
+    ull value;
     int next;
-    int value;
     int to;
-    Edge(int next, int to) : next(next), to(to) {}
+    Edge(int next, int to, ull value) : value(value), next(next), to(to) {}
   };
 
-  void add_edge(int from, int to) {
+  void add_edge(int from, int to, ull weight) {
     int pre_beg = operator[](from).edge_beg;
     operator[](from).edge_beg = edges.size();
-    edges.push_back(Edge(pre_beg, to));
+    edges.push_back(Edge(pre_beg, to, weight));
   }
 
   Edge get_edge(int edge_id) { return edges[edge_id]; }
@@ -35,19 +35,20 @@ public:
     Graph &graph = *this;
     graph[source].value = 0;
     std::queue<int> q;
+    q.push(source);
     while (!q.empty()) {
       int from = q.front();
       q.pop();
       Vertex &u = graph[from];
       u.visited++;
-      if (u.visited == graph.size()) {
+      if (u.visited == (int)graph.size()) {
         return false;
       }
-      for (Edge edge = get_edge(from); edge.to != -1;
+      for (Edge edge = get_edge(u.edge_beg); edge.to != -1;
            edge = get_edge(edge.next)) {
         Vertex &v = graph[edge.to];
         ull new_value = u.value + edge.value;
-        if (v.value < new_value) {
+        if (new_value < v.value) {
           v.value = new_value;
           q.push(edge.to);
         }
