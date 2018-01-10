282576c3e694ef4dc69323feb53601fb2cb0058a
FluorineDog <fluorine@gmail.com>
2018-01-10 14:55:28 +0800

    checkpoint

--- ComDesAndOrg/CPU/fix.sh
+++ ComDesAndOrg/CPU/fix.sh
@@ -1,2 +1,5 @@
-echo "v2.0 raw" > dog-test.hex && 
-cat dog-test-tmp.hex >> dog-test.hex
+echo "v2.0 raw" > tester.hex && 
+Mars dog-test.asm mc CompactDataAtZero dump .text HexText dog-test-tmp.hex > /dev/null && 
+cat dog-test-tmp.hex >> tester.hex && 
+rm dog-test-tmp.hex
+
