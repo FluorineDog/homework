718e0c9021d34d32f810b9e107636fa1e555034a
FluorineDog <fluorine@gmail.com>
2018-03-15 02:32:52 +0800

    wtf

--- poj/toy/stockseller.cpp
+++ poj/toy/stockseller.cpp
@@ -1,62 +1,83 @@
+// #include "../wheel.h"
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <limits>
 #include <queue>
+#include <tuple>
 #include <vector>
-#include <cassert>
-using std::cout;
-using std::endl;
+
+using std::make_pair;
 using std::priority_queue;
 using std::vector;
-using ll = std::pair<long long, int>;
-using std::make_pair;
+// NIL should be identity of moniod
+// sum, max_depth
+constexpr auto inf = std::numeric_limits<int>::max() / 4;
+using Data = std::pair<int, int>;
+Data dog_sum(Data data1, Data data2) {
+	auto[sum1, depth1] = data1;
+	auto[sum2, depth2] = data2;
+	int sum = sum1 + sum2;
+	int depth = std::min(depth1, depth2 + sum1);
+	return make_pair(sum, depth);
+}
+
+typedef Data T;
+#define FUNC(a, b) dog_sum((a), (b))
+#define NIL make_pair(0, inf)
+#define INIT make_pair(1, 1)
+#define FILP make_pair(-1, -1)
+using ll = long long;
+
+#include "../fenwick_tree/fenwick.h"
+// using std::cout;
+// using std::endl;
 
 int main() {
+	//
+	// Fenwick_tree
 	int N;
-	priority_queue<ll, std::vector<ll>, std::greater<ll>> data;
-	auto inf = std::numeric_limits<long long>::max() / 4;
-	// cout << "s = " << s << endl;
-	data.push(make_pair(inf, -1));
-	data.push(make_pair(inf, -1));
-	// cout << "s = " << data.top() << endl;
-	std::cin >> N;
-	long long sum = 0;
-	std::vector<int> day(N, 0);
+	cin >> N;
+	Fenwick lookup(N);
+	vector<std::pair<ll, int>> record;
+	lookup.fast_init();
 	for (int i = 0; i < N; ++i) {
 		ll x;
-		std::cin >> x.first;
-		x.second = i;
-		ll k = data.top();
-		if (x > data.top()) {
-			sum += (x.first - data.top().first);
-			data.pop();
-			data.push(x);
+		cin >> x;
+		record.push_back(make_pair(x, i));
+	}
+	ll sum = 0;
+	std::sort(record.begin(), record.end());
+	vector<int> answer(N);
+	vector<int> acc(N);
+	{
+		auto info = lookup.reduce(0, N);
+		cout << info.first << "# ";
+	}
+	for (int index = N; --index > 0;) {
+		auto[value, select_i] = record[index];
+		lookup.update(select_i, FILP);
+		auto info = lookup.reduce(0, N);
+		if (info.second < 0) {
+			sum -= value;
+			lookup.update(select_i, INIT);
+			answer[select_i] = value;
+			acc[select_i] = 1;
+		} else {
+			answer[select_i] = value;
+			acc[select_i] = -1;
+			sum += value;
 		}
-		data.push(x);
+		info = lookup.reduce(0, N);
+		cout << info.first << "$ ";
 	}
-	int left = data.size() - 1;
-	std::vector<long long> waster;
-	while (data.top().first != inf) {
-		auto p = data.top();
-		data.pop();
-		if(data.top() == p){
-			data.pop();
-		}
-		else{
-			waster.push_back(p.first);
-		}
+	int tmp = 0;
+	for(int i = 0; i < N; ++i){
+		tmp += acc[i];
+		acc[i] = tmp;
 	}
-	// for(auto x: waster){
-	// 	cout << x << ' ';
-	// }
-	// cout << endl;
-	assert(data.size() == 2);
-	for(int i = 0; i < waster.size() / 2; ++i){
-		sum += waster[i];
+	for(int i = 0; i < N; ++i){
+		cout << answer[i] << "(" << acc[i] << ") ";
 	}
-	for(int i = waster.size() / 2; i < waster.size(); ++i){
-		sum -= waster[i];
-	}
-	std::cout << sum << std::endl;
-	return 0;
+	cout << sum << endl;
 }
