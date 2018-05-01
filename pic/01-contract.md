e1efaa8ae522c69c753cc5d4561fea96f3e43f86
FluorineDog <fluorine@gmail.com>
2018-05-01 14:29:17 +0800

    02-half

---
+++ pic/01-contract.md
@@ -0,0 +1,2 @@
+1. 见代码01-contract.c0 (注意：由于没有方法检测函数是否会同时出现error，我使用了magic_number作为溢出后的返回值来检测，取消return magic_number;的注释即可)
+2. f(2, 1)在计算过程中会出现r=2的情况，不满足条件，会出现error
--- pic/fake_header.h
+++ pic/fake_header.h
@@ -1,1 +1,18 @@
-void error(char *);
+void error(char*);
+typedef char* string;
+int string_length(string s);                    /* number of characters in s */
+string string_join(string a, string b);         /* concatenate a and b */
+string string_sub(string a, int start, int end) /* substring a[start..end) */
+    //@requires 0 <= start && start <= end && end <= string_length(a);
+    ;
+
+/* lexicographic comparison; less than (-1), equal (0), greater than (1) */
+int string_compare(string a, string b)
+    //@ensures -1 <= \result && \result <= 1;
+    ;
+
+string string_fromint(int i);
+string string_frombool(bool b);
+string string_fromchar(char c)
+    //@requires c != '\0';
+    ;
