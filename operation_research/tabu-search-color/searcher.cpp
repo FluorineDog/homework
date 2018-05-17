d673612f0e47a3637e67eb9c142a70fbf4fa47a3
FluorineDog <fluorine@gmail.com>
2018-05-17 17:48:42 +0800

    49 searched

--- operation_research/tabu-search-color/searcher.cpp
+++ operation_research/tabu-search-color/searcher.cpp
@@ -1,12 +1,18 @@
 #include "TabuSearch.h"
 int main(int argc, char* argv[]) {
   int preset_color_count;
-  if (argc != 3) {
+  if (argc >= 3) {
     cin.redirect("data/DSJC500.5.col");
     preset_color_count = 49;
   } else {
     cin.redirect(argv[1]);
     preset_color_count = strtol(argv[2], nullptr, 10);
+  }
+
+  std::default_random_engine e(67);
+
+  if (argc >= 4) {
+    e.seed(strtol(argv[3], nullptr, 10));
   }
 
   char ch;
@@ -39,19 +45,22 @@ int main(int argc, char* argv[]) {
   }
   graph = graph.optimize();
   cout << "searching " << preset_color_count << endl;
-  std::default_random_engine e(67);
 
   TabuSearch engine(graph, preset_color_count);
+  int best = INF;
   for (int iter = 0; iter < 100000000; ++iter) {
     auto [v, c] = engine.pick_move(iter);
     int old_color = engine.shift(v, c);
     int cost = engine.getCurrentCost();
+    best = std::min(cost, best);
     if (iter % 100000 == 0) {
       cout                                     //
           << "iter: " << iter                  //
           << " " << cost                       //
+          << "<=>" << best                     //
           << "<=>" << engine.getHistoryCost()  //
           << endl;
+      best = INF;
     }
     engine.tabu(v, old_color, iter + e() % 10 + cost);
     if (cost == 0) {
