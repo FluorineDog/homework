add25c47a8e204dac98c1ecdece03c37408675d0
FluorineDog <fluorine@gmail.com>
2018-05-19 23:09:46 +0800

    update

--- pic/hw3-handout/run.sh
+++ pic/hw3-handout/run.sh
@@ -1,5 +1,6 @@
 #for i in "$@"; do sed -i 's:\(\w\)++:\1=\1+1:g' $i; done
 for i in "$@"; do sed -i 's:++\(\w\+\):\1+=1:g' $i; done
 for i in "$@"; do sed -i 's:\(//@.*[^; ] *$\):\1;:g' $i; done
-rlwrap coin -d "$@";
+cc0 -o a.out "$@" &&
+./a.out
 
