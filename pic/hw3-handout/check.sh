753fc8ea65c2a38a7ebf3d4960fbc7c2278a1f0d
FluorineDog <fluorine@gmail.com>
2018-05-19 22:11:27 +0800

    update

--- pic/hw3-handout/check.sh
+++ pic/hw3-handout/check.sh
@@ -1,2 +1,5 @@
+#for i in "$@"; do sed -i 's:\(\w\)++:\1=\1+1:g' $i; done
+for i in "$@"; do sed -i 's:++\(\w\):\1=\1+1:g' $i; done
 for i in "$@"; do sed -i 's:\(//@.*[^; ] *$\):\1;:g' $i; done
 rlwrap coin -d "$@";
+
