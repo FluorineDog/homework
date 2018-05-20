dbef970b4be8484da80e1d18da96bcb8a55a8c02
FluorineDog <fluorine@gmail.com>
2018-05-20 09:17:34 +0800

    init

--- pic/hw3-handout/check.sh
+++ pic/hw3-handout/check.sh
@@ -1,5 +1,5 @@
 #for i in "$@"; do sed -i 's:\(\w\)++:\1=\1+1:g' $i; done
 for i in "$@"; do sed -i 's:++\(\w\+\):\1+=1:g' $i; done
 for i in "$@"; do sed -i 's:\(//@.*[^; ] *$\):\1;:g' $i; done
-rlwrap coin -d "$@";
+rlwrap coin -d lib/* "$@";
 
